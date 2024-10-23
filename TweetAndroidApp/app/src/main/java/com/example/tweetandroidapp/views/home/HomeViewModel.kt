6a167ac0db5d186710b44bba2f46675c451fbc24
nikhil5642 <nikhilagrawal5642@gmail.com>
2024-10-23 20:49:23 +0530

    minor bug fix

--- TweetAndroidApp/app/src/main/java/com/example/tweetandroidapp/views/home/HomeViewModel.kt
+++ TweetAndroidApp/app/src/main/java/com/example/tweetandroidapp/views/home/HomeViewModel.kt
@@ -18,7 +18,7 @@ class HomeViewModel(private val tweetsRepository: TweetsRepository,
     }
 
     fun loadTweets() {
-        _tweets.value = tweetsRepository.getTweets().reversed()b.toList();
+        _tweets.value = tweetsRepository.getTweets().reversed().toList();
     }
 
 }
